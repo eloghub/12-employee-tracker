9d704aabe13b3a5cc2f84f63950d0a3bfa60611b
eloggit <elogtechmail@gmail.com>
2021-04-16 14:59:46 -0400

    moved 3 primary keys to their own lines

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 USE employees_db;
 
-CREATE TABLE department(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+CREATE TABLE department
+(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE role(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
+    PRIMARY KEY(id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     FOREIGN KEY (role_id) REFERENCES role(id),
-    manager_id INT NOT NULL,
-    FOREIGN KEY (manager_id) REFERENCES role(title)
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES role(title),
+    PRIMARY KEY(id)
 );

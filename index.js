d91644ce2a247b38f664d82782765dbd81974c5c
eloggit <elogtechmail@gmail.com>
2021-04-16 14:54:55 -0400

    added employee table to database, need to make sure role_id, manager_id and their foreign keys were included correctly

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ CREATE TABLE role(
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT NOT NULL,
+    FOREIGN KEY (manager_id) REFERENCES role(title)
+);
